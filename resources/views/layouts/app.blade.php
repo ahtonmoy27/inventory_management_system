f04cfc8c6f39145f30dcd202b517f70d7ae1b892
Tonmoy <hasnatiucse@gmail.com>
2026-02-23 21:34:53 +0600

    fixing Dashboard issue

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -62,7 +62,7 @@
     <nav class="navbar navbar-expand-lg navbar-light bg-white border-bottom">
         <div class="container-fluid">
             <a class="navbar-brand" href="{{ route('dashboard') }}">
-                <i class="bi bi-box-seam"></i> InventoryPro
+                <i class="bi bi-box-seam"></i> Inventory Management System
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
                 <span class="navbar-toggler-icon"></span>
